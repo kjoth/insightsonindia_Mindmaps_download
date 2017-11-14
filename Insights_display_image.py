fc6741629e5cc044d468edb8ad1860267948f992
kjoth <karthickjothimani@gmail.com>
2017-11-14 11:50:09 +0530

    Update Insights_display_image.py

--- Insights_display_image.py
+++ Insights_display_image.py
@@ -1,7 +1,7 @@
 import random
 import os
 
-dir_folder = "/home/karthik/Desktop/CSE/InsightsonIndia/"
+dir_folder = "/home/karthik/Desktop/CSE/InsightsonIndia/Mindmaps/To_be_used"
 
 rnd_dir = dir_folder + random.choice(os.listdir(dir_folder))
 a = rnd_dir + '/' +random.choice(os.listdir(rnd_dir))
